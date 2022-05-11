193ceb53026796aeaf19ba15d45923b5a4db7af9
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-05-10 20:35:35 -0600

    fun

---
+++ src/multiplication_table.java
@@ -0,0 +1,14 @@
+
+public class multiplication_table
+ {
+	public static void main(String[] args) {
+	  for (int i = 2; i<4; i++)
+	  {
+		  for(int j = 1; j<10; j++) {	  
+		  System.out.println(i + "*" + j +"=" + i*j);
+		  System.out.println(i + "*" + j +"=" + i*j);		  
+		  
+	  }
+	}
+}
+}
