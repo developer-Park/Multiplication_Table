083799aeac41c41a6768fb1e3f57476e7a600f3b
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-05-10 21:20:37 -0600

    good

---
+++ src/multiplication_arrary.java
@@ -0,0 +1,36 @@
+
+public class multiplication_arrary {
+
+	public static void main(String[] args) {
+		int[] result = new int[9];
+		for(int i = 0; i < result.length; i++) {
+				result[i] = 2 * (i + 1);
+		}
+		for(int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+		
+		result = new int[9];
+		for(int i = 0; i < result.length; i++) {
+			result[i] = 3* (i + 1);
+		}
+		for(int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+		
+		int array[][] = new int[10][10];
+		for(int i = 0; i<array.length; i++) {
+			for(int j = 0; j<array[i].length; j++) {
+			array[i][j] = i * j;
+			}
+		}
+		for(int i = 2; i < array.length; i++) {
+			for(int j = 1; j < array[i].length; j++) {
+				System.out.println(array[i][j]);
+		}
+		}
+		
+		
+	}
+
+}
